ef3e2a0519ae7e3f7a8090d044c6035a0d3ce3e7
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-28 15:41:52 +0330

    feat(dashboard): create wellcom box component

--- src/components/pages/dashboard/WelcomeBox/index.tsx
+++ src/components/pages/dashboard/WelcomeBox/index.tsx
@@ -1,5 +1,21 @@
-const WelcomeBox = ()=>{
-    return(
-        <></>
-    )
-}
+import { User } from "@/types/user";
+import Image from "next/image";
+
+interface WelcomeBoxProps extends Omit<User, "email"> {}
+
+const WelcomeBox = ({ name, picture }: WelcomeBoxProps) => {
+  return (
+    <div className="flex flex-col items-center gap-24">
+      <Image
+        src={picture}
+        alt={`${name}'s profile picture`}
+        width={120}
+        height={120}
+        className="rounded-full object-cover"
+      />
+      <p className="text-xl font-bold text-center">Hello {name}!</p>
+    </div>
+  );
+};
+
+export default WelcomeBox
