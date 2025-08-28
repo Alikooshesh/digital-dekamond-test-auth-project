13c77a251d216e10bb7bea761d6df5e46b7f5b5c
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-28 15:42:06 +0330

    feat(dashboard): finish dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,31 @@
-const Page = ()=>{
-    return(
-        <></>       
-    )
-}
+"use client";
 
-export default Page
+import Button from "@/components/general/Button";
+import WelcomeBox from "@/components/pages/dashboard/WelcomeBox";
+import { useUser } from "@/hooks/useUser";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+const Page = () => {
+  const router = useRouter();
+  const { user, logout } = useUser();
+
+  useEffect(() => {
+    if (!user) {
+      router.push("/login");
+    }
+  }, [[user, router]]);
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+      <div className="w-full max-w-500 mx-auto px-8 flex flex-col gap-32">
+        {!user ? <p>Bye Bye ...</p> : <WelcomeBox {...user} />}
+        <Button onClick={logout} variant="outline">
+            Logout
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
