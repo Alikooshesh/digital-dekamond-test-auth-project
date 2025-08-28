9b2f4e74fd1d017660b00fd507a6d7e7af3f67cc
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-28 13:18:01 +0330

    feat: create folder and files structure

---
+++ src/app/dashboard/layout.tsx
@@ -0,0 +1,13 @@
+const Layout = ({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>)=>{
+    return(
+        <>
+            {children}
+        </>
+    )
+}
+
+export default Layout
