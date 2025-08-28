9e1717f9fe40de06b919f7203db476f37dbf67ff
Ali Kooshesh <alikooshesh007@gmail.com>
2025-08-28 13:31:23 +0330

    feat(tailwind): add custom variables to theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,7 +6,43 @@ module.exports = {
     './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: '#6B21A8',
+          light: '#9333EA',
+          dark: '#4C1D95',
+        },
+        background: '#FFFFFF',
+        onBackground: '#1F2937',
+      },
+      spacing: {
+        2: '2px',
+        4: '4px',
+        6: '6px',
+        8: '8px',
+        10: '10px',
+        12: '12px',
+        14: '14px',
+        16: '16px',
+        18: '18px',
+        20: '20px',
+        22: '22px',
+        24: '24px',
+      },
+      width: {
+        500: '500px',
+      },
+      height: {
+        500: '500px',
+      },
+      maxWidth: {
+        500: '500px',
+      },
+      maxHeight: {
+        500: '500px',
+      },
+    },
   },
   plugins: [],
-}
+};
